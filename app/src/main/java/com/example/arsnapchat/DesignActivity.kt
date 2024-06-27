c016e0f3c7aca41759f8283f70a9594edfb71ac5
PREM SHANKAR UPADHYAY <50406436+premanish9@users.noreply.github.com>
2024-06-27 18:38:49 +0530

    1. save and open of image done 2. save and open of video done

--- app/src/main/java/com/example/arsnapchat/DesignActivity.kt
+++ app/src/main/java/com/example/arsnapchat/DesignActivity.kt
@@ -198,438 +198,6 @@ fun GreetingPreview() {
 }
 
 
-@Composable
-fun GreetingSection(name: String = "Geeks") {
-    Row(
-        horizontalArrangement = Arrangement.SpaceBetween,
-        verticalAlignment = Alignment.CenterVertically,
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(15.dp)
-    ) {
-        Column(verticalArrangement = Arrangement.Center) {
-            // heading text view
-            Text(text = "Good morning, $name", style = MaterialTheme.typography.headlineSmall)
-            Text(text = "We wish you have a good day!", style = MaterialTheme.typography.bodySmall)
-        }
-        // search icon
-        Icon(
-            painter = painterResource(id = R.drawable.baseline_search_24),
-            contentDescription = "Search",
-            tint = Color.White,
-            modifier = Modifier.size(24.dp)
-        )
-    }
-}
-
-// This is how we can create chip seaction at the top of app
-
-@Composable
-fun ChipSection(
-    // function with single argument
-    chips: List<String>
-) {
-    var selectedChipIndex by remember {
-        // it will not update the string
-        // but save and it will helpful for us
-        mutableStateOf(0)
-    }
-    LazyRow {
-        items(chips.size) {
-            androidx.compose.foundation.layout.Box(contentAlignment = Alignment.Center,
-                modifier = Modifier
-                    .padding(start = 15.dp, top = 15.dp, bottom = 15.dp)
-                    .clickable {
-                        selectedChipIndex = it
-                    }
-                    .clip(RoundedCornerShape(10.dp))
-                    .background(
-                        // this is basic condition for selected chip index
-
-                        if (selectedChipIndex == it) Color.Green
-                        else Color.Gray
-                    )
-                    .padding(15.dp)
-            ) {
-                Text(text = chips[it], color = Color.White)
-            }
-        }
-    }
-
-}
-
-// This function is for suggestion secation
-@Composable
-fun SuggestionSection(color: Color = Color.Blue) {
-    Row(
-        verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.SpaceBetween,
-        modifier = Modifier
-            .padding(15.dp)
-            .clip(RoundedCornerShape(10.dp))
-            .background(color)
-            .padding(horizontal = 15.dp, vertical = 20.dp)
-            .fillMaxWidth()
-
-    ) {
-        Column {
-            // here are two text views or we can say only text
-
-            Text(
-                text = "Daily Coding",
-                // it can be litile bit confusing but
-                // it is just text style alternate
-                // of fontfamily in XML
-                style = MaterialTheme.typography.headlineMedium
-            )
-            Text( // same as above
-                text = "do at least . 3-10 problems / day",
-                style = MaterialTheme.typography.bodySmall,
-                color = Color.White
-            )
-
-        }
-
-        androidx.compose.foundation.layout.Box( // box containing icon
-            contentAlignment = Alignment.Center, modifier = Modifier
-                .size(40.dp)
-                .clip(
-                    CircleShape
-                )
-                .background(Color.Green)
-                .padding(10.dp)
-        ) {
-            Icon(
-                painter = painterResource(id = R.drawable.baseline_play_circle_24),
-                contentDescription = "Play",
-                tint = Color.White,
-                modifier = Modifier.size(16.dp)
-            )
-        }
-
-    }
-}
-
-@Composable
-// this function tells us that
-// how menu item should look like
-fun BottomMenu(
-    items: List<BottomMenuContent>,
-    modifier: Modifier = Modifier,
-    activeHighlightColor: Color = Color.Green,
-    activeTextColor: Color = Color.White,
-    inactiveTextColor: Color = Color.White,
-    initialSelectedItemIndex: Int = 1
-) {
-    var selectedItemIndex by remember {
-        mutableStateOf(initialSelectedItemIndex)
-    }
-    Row(
-        horizontalArrangement = Arrangement.SpaceAround,
-        verticalAlignment = Alignment.CenterVertically,
-        modifier = modifier
-            .fillMaxWidth()
-            .background(Color.Blue)
-            .padding(15.dp)
-    ) {
-        // it is basically what we should have
-        // for creating an element of BottomMenuItem
-        items.forEachIndexed { index, item ->
-            BottomMenuItem(
-                item = item, isSelected = index == selectedItemIndex,
-                activeHighlightColor = activeHighlightColor,
-                inactiveTextColor = inactiveTextColor
-            ) {
-                selectedItemIndex = index
-            }
-        }
-
-
-    }
-}
-
-
-// it's basically how menu item should look like
-@Composable
-fun BottomMenuItem(
-    item: BottomMenuContent,
-    isSelected: Boolean = false,
-    activeHighlightColor: Color = Color.Green,
-    activeTextColor: Color = Color.White,
-    inactiveTextColor: Color = Color.Blue,
-    onItemClick: () -> Unit
-) {
-
-    Column(horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center,
-        modifier = Modifier.clickable { onItemClick }) {
-// here are some peremetens
-        // for how elements will align
-        androidx.compose.foundation.layout.Box(
-            contentAlignment = Alignment.Center,
-            modifier = Modifier
-                .clip(RoundedCornerShape(10.dp))
-                .background(if (isSelected) activeHighlightColor else Color.Transparent)
-                .padding(10.dp)
-        ) {
-            Icon(
-                painter = painterResource(id = item.iconId),
-                contentDescription = item.title,
-                tint = if (isSelected) activeTextColor else inactiveTextColor,
-                modifier = Modifier.size(20.dp)
-            )
-        }
-
-        Text(
-            text = item.title,
-            // it's basic condition
-            color = if (isSelected) activeTextColor else inactiveTextColor
-        )
-
-    }
-
-}
-
-@ExperimentalFoundationApi
-@Composable
-// here we have just passed the list of courses
-fun CourseSection(courses: List<Course>) {
-    Column(modifier = Modifier.fillMaxWidth()) {
-        Text(
-            text = "courses",
-            style = MaterialTheme.typography.headlineSmall, modifier = Modifier.padding(15.dp)
-        )
-        // we have used lazyVertically grid
-        LazyVerticalGrid(
-            columns = GridCells.Fixed(2),
-            contentPadding = PaddingValues(start = 7.5.dp, end = 7.5.dp, bottom = 100.dp),
-            modifier = Modifier.fillMaxHeight()
-        ) {
-            items(courses.size) {
-                // here we have to define how one of these item is look like
-                // we will tell after defining item design
-                // let me comment it for now and after
-                // creating you just have to remove
-
-                CourseItem(course = courses[it])
-            }
-
-        }
-
-    }
-
-
-}
-
-
-@Composable
-fun CourseItem(course: Course) {
-    BoxWithConstraints( // Box with some attributes
-        modifier = Modifier
-            .padding(7.5.dp)
-            .aspectRatio(1f)
-            .clip(RoundedCornerShape(10.dp))
-            .background(Color(R.color.colorAccent))
-    ) {
-        val width = constraints.maxWidth
-        val height = constraints.maxHeight
-        // setting 5 points for medium
-        // color or we can say for another
-        // Medium colored path
-        val mediumColoredPoint1 = Offset(0f, height * 0.3f)
-        val mediumColoredPoint2 = Offset(width * 0.1f, height * 0.35f)
-        val mediumColoredPoint3 = Offset(width * 0.4f, height * 0.05f)
-        val mediumColoredPoint4 = Offset(width * 0.75f, height * 0.7f)
-        val mediumColoredPoint5 = Offset(width * 1.4f, -height.toFloat())
-        // joining points to make curves with the help of path class
-        // path file that we have created earlier
-        // having function that just help to reduce our code
-        // and the function is standardQuadFromTo(m1,m2) taking
-        // two peramente and connect them
-        val mediumColoredPath = Path().apply {
-            moveTo(mediumColoredPoint1.x, mediumColoredPoint1.y)
-            standardQuadFromTo(mediumColoredPoint1, mediumColoredPoint2)
-            standardQuadFromTo(mediumColoredPoint2, mediumColoredPoint3)
-            standardQuadFromTo(mediumColoredPoint3, mediumColoredPoint4)
-            standardQuadFromTo(mediumColoredPoint4, mediumColoredPoint5)
-            lineTo(width.toFloat() + 100f, height.toFloat() + 100f)
-            lineTo(-100f, height.toFloat() + 100f)
-            close()
-        }
-
-        // it's another part of that
-        // texture with light color
-        // Light colored path
-        val lightPoint1 = Offset(0f, height * 0.35f)
-        val lightPoint2 = Offset(width * 0.1f, height * 0.4f)
-        val lightPoint3 = Offset(width * 0.3f, height * 0.35f)
-        val lightPoint4 = Offset(width * 0.65f, height.toFloat())
-        val lightPoint5 = Offset(width * 1.4f, -height.toFloat())
-
-        val lightColoredPath = Path().apply {
-            moveTo(lightPoint1.x, lightPoint1.y)
-            standardQuadFromTo(lightPoint1, lightPoint2)
-            standardQuadFromTo(lightPoint2, lightPoint3)
-            standardQuadFromTo(lightPoint3, lightPoint4)
-            standardQuadFromTo(lightPoint4, lightPoint5)
-            lineTo(width.toFloat() + 100f, height.toFloat() + 100f)
-            lineTo(-100f, height.toFloat() + 100f)
-            close()
-        }
-
-// canvas is used when we
-        // want to draw something
-        androidx.compose.foundation.Canvas(modifier = Modifier.fillMaxSize()) {
-            drawPath(// function for drawing paths
-                // just pass the path
-                path = mediumColoredPath, color = course.mediumColor
-            )
-            drawPath(// it's for the lighter path
-                path = lightColoredPath, color = course.lightColor
-            )
-        }
-
-// so , we have done with texture and
-        // now just creating box and other things
-        // box containing course elements
-        androidx.compose.foundation.layout.Box(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(15.dp)
-        ) {
-            Text(
-                text = course.title,
-                style = MaterialTheme.typography.headlineMedium,
-                lineHeight = 26.sp,
-                modifier = Modifier.align(Alignment.TopStart)
-            )
-            Icon(
-                painter = painterResource(id = course.iconId),
-                contentDescription = course.title,
-                tint = Color.White,
-                modifier = Modifier.align(Alignment.BottomStart)
-            )
-            Text(
-                text = "Start",
-                color = Color.White,
-                fontSize = 14.sp,
-                fontWeight = FontWeight.Bold,
-                modifier = Modifier
-                    .clickable {
-                        // Handle the clicks
-                    }
-                    .align(Alignment.BottomEnd)
-                    .clip(
-                        RoundedCornerShape(10.dp)
-                    )
-                    .background(Color.Green)
-                    .padding(vertical = 6.dp, horizontal = 15.dp)
-            )
-
-        }
-    }
-}
-
-fun Path.standardQuadFromTo(from: Offset, to: Offset) {
-    // this function is basically draw
-    // a line to our second point and
-    // also smooth on that line and make it curve
-    quadraticBezierTo(from.x, from.y, abs(from.x + to.x) / 2f, abs(from.y + to.y) / 2f)
-}
-
-@ExperimentalFoundationApi
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    // HomeSccreen() this is the most outer box that will
-    // contain all the views,buttons,chips,etc.
-    Box(
-        modifier = Modifier
-            .background(Color(R.color.Beige2))
-            .fillMaxSize()
-    ) {
-        Column {
-            // this is the function for header
-            GreetingSection()
-            // it's for chipsSecation, and pass
-            // as many strings as you want
-            ChipSection(
-                chips = listOf(
-                    "Data structure",
-                    "Algorithm",
-                    "competitive programming",
-                    "python"
-                )
-            )
-            // function for suggestionSection
-            SuggestionSection()
-            // this is for course secation
-            CourseSection(
-                // function require list of courses and
-                // one course contain 5 attributes
-                courses = listOf(
-                    Course(
-                        title = "greek of the year",
-                        R.drawable.baseline_headphones_24,
-                        Color.Blue,
-                        Color.DarkGray,
-                        Color.LightGray
-                    ),
-                    Course(
-                        title = "How does AI Works",
-                        R.drawable.baseline_videocam_24,
-                        Color.Green,
-                        Color.Black,
-                        Color.Black
-                    ),
-                    Course(
-                        title = "Advance python Course",
-                        R.drawable.baseline_play_circle_24,
-                        Color(R.color.skyblue1),
-                        Color(R.color.skyblue2),
-                        Color(R.color.skyblue3)
-                    ),
-                    Course(
-                        title = "Advance Java Course",
-                        R.drawable.baseline_headphones_24,
-                        Color(R.color.Beige1),
-                        Color(R.color.Beige2),
-                        Color(R.color.Beige3)
-                    ),
-                    Course(
-                        title = "prepare for aptitude test",
-                        R.drawable.baseline_play_circle_24,
-                        Color(R.color.OrangeYellow1),
-                        Color(R.color.OrangeYellow2),
-                        Color(R.color.OrangeYellow3)
-                    ),
-                    Course(
-                        title = "How does AI Works",
-                        R.drawable.baseline_videocam_24,
-                        Color(R.color.LightGreen1),
-                        Color(R.color.LightGreen2),
-                        Color(R.color.LightGreen3)
-                    ),
-
-                    )
-            )
-
-
-        }
-
-        // this is the final one that is bottomMenu
-        BottomMenu(
-            items = listOf(
-                // having 5 instances
-                BottomMenuContent("Home", R.drawable.baseline_home_24),
-                BottomMenuContent("explore", R.drawable.baseline_explore_24),
-                BottomMenuContent("dark mode", R.drawable.baseline_shield_moon_24),
-                BottomMenuContent("videos", R.drawable.baseline_videocam_24),
-                BottomMenuContent("Profile", R.drawable.baseline_self_improvement_24),
-            ), modifier = Modifier.align(Alignment.BottomCenter)
-        )
-
-    }
-}
 
 @Composable
 @ExperimentalFoundationApi
@@ -673,30 +241,7 @@ fun replaceTextAtIndex(contents: List<Content>, index: Int?, newText: String): L
 
 }
 
-fun saveImageToGallery(context: Context, bitmap: Bitmap, displayName: String): Uri? {
-    val resolver = context.contentResolver
-    val contentValues = ContentValues().apply {
-        put(MediaStore.MediaColumns.DISPLAY_NAME, displayName)
-        put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg")
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-            put(MediaStore.MediaColumns.RELATIVE_PATH, "Pictures/MyAppImages")
-        }
-    }
 
-    val imageUri: Uri? = resolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues)
-    imageUri?.let {
-        val outputStream: OutputStream? = resolver.openOutputStream(it)
-        outputStream?.use { stream ->
-            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream)
-        }
-    }
-    return imageUri
-}
-
-fun loadImageFromUri(context: Context, uri: Uri): Bitmap? {
-    val inputStream = context.contentResolver.openInputStream(uri)
-    return BitmapFactory.decodeStream(inputStream)
-}
 
 @Composable
 fun ToolbarSection() {
@@ -1637,16 +1182,7 @@ fun loadBitmapFromUri(context: Context, uri: Uri): Bitmap? {
     }
 }
 
-fun getFontFromAssetsByName(context: Context, fontName: String): FontFamily {
-    val typeface = Typeface.createFromAsset(context.assets, "fonts/$fontName")
-    return FontFamily(typeface)
-}
 
-@Composable
-fun LoadImageFromStorage(filePath: String): Painter {
-    val imagePainter = rememberAsyncImagePainter(model = File(filePath))
-    return imagePainter
-}
 
 @Composable
 fun BottomMenuColumn(
@@ -1847,7 +1383,7 @@ fun BottomMenuColumn(
 
                         val serializedData = serializeEditorContent(editorContent)
 
-                        Log.i("DesignActivity", "save $serializedData")
+                       // Log.i("DesignActivity", "save $serializedData")
 
                         saveToSharedPreferences(context = context, "editorContent", serializedData)
 
@@ -1892,7 +1428,7 @@ fun BottomMenuColumn(
 
 
                                 it.chartData?.let { it1 -> onBarChartDataChange(it1) }
-                                showDialog = false
+                               // showDialog = false
 
                                /* Log.i(
                                     "DesignActivity",
@@ -2205,6 +1741,9 @@ fun ImageListAlertDialog(
 }
 
 
+
+
+
 @Composable
 fun ItemCard(item: BottomMenuContent) {
     Row(
@@ -2237,4 +1776,38 @@ fun ItemList(items: List<BottomMenuContent>) {
     }
 }
 
+fun saveImageToGallery(context: Context, bitmap: Bitmap, displayName: String): Uri? {
+    val resolver = context.contentResolver
+    val contentValues = ContentValues().apply {
+        put(MediaStore.MediaColumns.DISPLAY_NAME, displayName)
+        put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            put(MediaStore.MediaColumns.RELATIVE_PATH, "Pictures/MyAppImages")
+        }
+    }
 
+    val imageUri: Uri? = resolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues)
+    imageUri?.let {
+        val outputStream: OutputStream? = resolver.openOutputStream(it)
+        outputStream?.use { stream ->
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream)
+        }
+    }
+    return imageUri
+}
+
+fun loadImageFromUri(context: Context, uri: Uri): Bitmap? {
+    val inputStream = context.contentResolver.openInputStream(uri)
+    return BitmapFactory.decodeStream(inputStream)
+}
+
+fun getFontFromAssetsByName(context: Context, fontName: String): FontFamily {
+    val typeface = Typeface.createFromAsset(context.assets, "fonts/$fontName")
+    return FontFamily(typeface)
+}
+
+@Composable
+fun LoadImageFromStorage(filePath: String): Painter {
+    val imagePainter = rememberAsyncImagePainter(model = File(filePath))
+    return imagePainter
+}
